8a514a9258a14f897b3dc221a52fa1eaeb405d01
Michael Ngo <mn542@cornell.edu>
2026-04-20 13:43:31 -0400

    add loop in diary

--- interface/mvp_gpt.py
+++ interface/mvp_gpt.py
@@ -1,5 +1,5 @@
 import json
-from diary import start_diary, get_entry, gpt_diary_response, put_reply
+from diary import start_diary, get_entry, gpt_diary_response, put_reply, ask_to_continue, end_diary
 
 
 def run_diary():
@@ -7,7 +7,11 @@ def run_diary():
     entry = get_entry()
     output_txt = gpt_diary_response(entry)
     put_reply(output_txt)
-
+    while ask_to_continue(): # loops if user has more to say
+        entry = get_entry()
+        output_txt = gpt_diary_response(entry)
+        put_reply(output_txt)
+    end_diary()
 
 if __name__ == "__main__":
     run_diary()
